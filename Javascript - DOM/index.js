cf10eff3ae605c8d91b05385ba802e313f85c916
Zahra Farahmand <zfarahmand1992@gmail.com>
2023-03-07 13:07:38 +0330

    JsDOM-selectors

---
+++ Javascript - DOM/index.js
@@ -0,0 +1,15 @@
+// let heading = document.firstElementChild.lastElementChild.firstElementChild;
+// heading.innerHTML = "Goodbye";
+// heading.style.color = "red";
+// document.querySelector("input").click();
+
+/*
+Challenge Solution:
+*/
+// var listElement = document.querySelector("ul").lastElementChild;
+// listElement.innerHTML = "Zahra Farahmand";
+
+/*
+Challenge Solution:
+*/
+document.querySelector("#list .item:first-child a").style.color = "red";
