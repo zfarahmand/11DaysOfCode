25943e5bb1a5b2a8ff940edbdc1687aac7270a6e
Zahra Farahmand <zfarahmand1992@gmail.com>
2023-03-14 20:16:37 +0330

    expressjs-myexpressserver

--- Expressjs - My Express Server/server.js
+++ Expressjs - My Express Server/server.js
@@ -5,6 +5,14 @@ app.get("/" , function(request , response) {
     response.send("<h1>Hello, baby!</h1>");
 });
 
+app.get("/contact" , function(request , response) {
+    response.send("Contact me at: zfarahmand1992@gmail.com");
+});
+
+app.get("/about" , function(request , response) {
+    response.send("Hello! It's me myself!");
+});
+
 app.listen(3000 , function() {
     console.log("Server is running on port 3000.");
 });
