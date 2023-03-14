ab2b8a5fad32aadfeb3dbce682657bebc52ed9b5
Zahra Farahmand <zfarahmand1992@gmail.com>
2023-03-14 14:55:40 +0330

    nodejs-intro-finish

--- Nodejs - Intro/index.js
+++ Nodejs - Intro/index.js
@@ -1,2 +1,19 @@
+//jshint esversion:6
+
+
+// Using Nodejs Native Modules
 const fs = require('fs');
-console.log(fs);
+// console.log(fs);
+// fs.copyFileSync('file1.txt' , 'file2.txt');
+
+
+// Using Nodejs External Modules
+const superheroes = require('superheroes');
+let superHero = superheroes.random();
+
+
+// CHALLENGE
+const supervillains = require('supervillains');
+let superVillain = supervillains.random();
+console.log(superVillain);
+
