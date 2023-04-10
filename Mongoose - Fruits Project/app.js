a70c91a535546ea68fba0fe25379f4b57e3bef1b
Zahra Farahmand <zfarahmand1992@gmail.com>
2023-04-10 20:15:43 +0430

    mongoose-fruitsproject-data-validation

--- Mongoose - Fruits Project/app.js
+++ Mongoose - Fruits Project/app.js
@@ -6,52 +6,66 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB');
 
+
+  ///////////////////////////////////////////////// Insertion //////////////////////////////////
   const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+      type: String,
+      required: [true , "You have forgotten to specify the name!"]
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 10
+    },
     review: String
   });
   const Fruit = mongoose.model("Fruit", fruitSchema);
 
-  const apple = new Fruit({
-    name: "Apple",
-    rating: 8,
-    review: "Great fruit when you're hungry!"
-  });
+  // const apple = new Fruit({
+  //   name: "Apple",
+  //   rating: 40,
+  //   review: "Great fruit when you're hungry!"
+  // });
 
-  // await apple.save();
-
-  const personSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-  });
-
-  const Person = new mongoose.model("Person", personSchema);
-
-  const person = new Person({
-    name: "John",
-    age: 37
-  });
-
-  await person.save();
-
-  const kiwi = new Fruit({
-    name: "Kiwi",
-    rating: 9,
-    review: "Fairly sour."
-  });
-
-  const orange = new Fruit({
-    name: "Orange",
+  const peach = new Fruit({
     rating: 10,
-    review: "Has a beautiful color!"
+    review: "Peaches are truely lovely!"
   });
 
-  const banana = new Fruit({
-    name: "Banana",
-    rating: 10,
-    review: "Nice texture and easy to eat!"
-  });
+  await peach.save();
+
+  // const personSchema = new mongoose.Schema({
+  //   name: String,
+  //   age: Number
+  // });
+
+  // const Person = new mongoose.model("Person", personSchema);
+
+  // const person = new Person({
+  //   name: "John",
+  //   age: 37
+  // });
+
+  // await person.save();
+
+  // const kiwi = new Fruit({
+  //   name: "Kiwi",
+  //   rating: 9,
+  //   review: "Fairly sour."
+  // });
+
+  // const orange = new Fruit({
+  //   name: "Orange",
+  //   rating: 10,
+  //   review: "Has a beautiful color!"
+  // });
+
+  // const banana = new Fruit({
+  //   name: "Banana",
+  //   rating: 10,
+  //   review: "Nice texture and easy to eat!"
+  // });
 
   // Fruit.insertMany([
   //   apple, kiwi, orange,banana    
@@ -64,15 +78,18 @@ async function main() {
 
   ////////////////////////////// Query/////////////////////////////////////////////////
 
-Fruit.find().select('name').exec().then(function(fruits) {
-  mongoose.connection.close();  
+// Fruit.find().select('name').exec().then(function(fruits) {
+//   mongoose.connection.close();  
   
-  fruits.forEach(function(fruit) {
-      console.log(fruit.name);
-    });
-  }).catch(function(err) {
-    console.log(err);
-  });
+//   fruits.forEach(function(fruit) {
+//       console.log(fruit.name);
+//     });
+//   }).catch(function(err) {
+//     console.log(err);
+//   });
 
   // Tank.find({ size: 'small' }).where('createdDate').gt(oneYearAgo).exec(callback);
+
+
+
 }
