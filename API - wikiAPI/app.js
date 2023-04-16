2e562df8362251056ddf3de39e30a35cebcd5b91
Zahra Farahmand <zfarahmand1992@gmail.com>
2023-04-17 01:02:47 +0430

    api-wiki-add-articles-routes

--- API - wikiAPI/app.js
+++ API - wikiAPI/app.js
@@ -22,6 +22,17 @@ async function main() {
 
 const getRoutes = async () => {
     const Article = await createArticleSchema();
+
+    app.route("/articles")
+    .get(async (req , res) => {
+        await getArticles(Article.model , res);
+    })
+    .post(async (req , res) => {
+        await createArticle(Article.model , req.body.title , req.body.content , res);
+    })
+    .delete(async (req , res) => {
+        await deleteArticles(Article.model , res);
+    });
 }
 
 const createArticleSchema = async () => {
@@ -40,6 +51,36 @@ const createArticleSchema = async () => {
         schema: articleSchema,
         model: articleModel
     }
+}
+
+const getArticles = async (articleModel , res) => {
+    await articleModel.find().exec()
+    .then((articles) => {
+        res.send(articles);
+    }).catch((err) => {
+        res.send(err);
+    });
+}
+
+const createArticle = async (articleModel , articleTitle , articleContent , res) => {
+    const newArticle = new articleModel({
+        title: articleTitle,
+        content: articleContent
+    });
+    return await newArticle.save().then(() => {
+        res.send("Succesfully added the article.");
+    }).catch((err) => {
+        res.send(err);
+    });
+}
+
+const deleteArticles = async (articleModel , res) => {
+    return await articleModel.deleteMany().then(() => {
+        res.send("Succesfully deleted all the articles")
+        .catch((err) => {
+            res.send(err);
+        });
+    })
 }
 
 app.listen(3000 , () => {
