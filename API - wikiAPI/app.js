908a18577804928f3af3f2783986b1fac93e8ef0
Zahra Farahmand <zfarahmand1992@gmail.com>
2023-04-17 02:41:33 +0430

    api-wiki-article-routes

--- API - wikiAPI/app.js
+++ API - wikiAPI/app.js
@@ -33,6 +33,20 @@ const getRoutes = async () => {
     .delete(async (req , res) => {
         await deleteArticles(Article.model , res);
     });
+
+    app.route("/articles/:title")
+    .get(async (req , res) => {
+        await getArticle(Article.model , req.params.title , res);
+    })
+    .put(async (req , res) => {
+        await overwriteArticle(Article.model , req.params.title , req.body.title , req.body.content , res);
+    })
+    .patch(async (req , res) => {
+        await updateArticle(Article.model , req.params.title , req.body , res);
+    })
+    .delete(async (req, res) => {
+        await deleteArticle(Article.model , req.params.title , res);
+    });
 }
 
 const createArticleSchema = async () => {
@@ -62,6 +76,16 @@ const getArticles = async (articleModel , res) => {
     });
 }
 
+const getArticle = async (articleModel , articleTitle , res) => {
+    await articleModel.findOne({title: articleTitle}).exec()
+    .then((article) => {
+        res.send(article);
+    })
+    .catch((err) => {
+        res.send(err);
+    });
+}
+
 const createArticle = async (articleModel , articleTitle , articleContent , res) => {
     const newArticle = new articleModel({
         title: articleTitle,
@@ -74,6 +98,27 @@ const createArticle = async (articleModel , articleTitle , articleContent , res)
     });
 }
 
+const overwriteArticle = async (articleModel , articleTitle , newArticleTitle , newArticleContent , res) => {
+    return await articleModel.findOneAndUpdate({title: articleTitle} , 
+         {title: newArticleTitle , content: newArticleContent}, {overwrite: true})
+    .then(() => {
+        res.send("Succesfully updated the article.");
+    })
+    .catch((err) => {
+        res.send(err);
+    });
+}
+
+const updateArticle = async (articleModel , articleTitle , newArticle , res) => {
+    return await articleModel.findOneAndUpdate({title: articleTitle} , newArticle)
+    .then(() => {
+        res.send("Succesfully updated the article.");
+    })
+    .catch((err) => {
+        res.send(err);
+    });
+}
+
 const deleteArticles = async (articleModel , res) => {
     return await articleModel.deleteMany().then(() => {
         res.send("Succesfully deleted all the articles")
@@ -81,6 +126,16 @@ const deleteArticles = async (articleModel , res) => {
             res.send(err);
         });
     })
+}
+
+const deleteArticle = async (articleModel , articleTitle , res) => {
+    return await articleModel.findOneAndDelete({title: articleTitle})
+    .then(() => {
+        res.send("Succesfully deleted the article.");
+    })
+    .catch((err) => {
+        res.send(err);
+    });
 }
 
 app.listen(3000 , () => {
