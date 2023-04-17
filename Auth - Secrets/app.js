ee823f08058dfd9ac28b2d4389cd8892b1b4c63a
Zahra Farahmand <zfarahmand1992@gmail.com>
2023-04-17 18:25:45 +0430

    auth-secrets-email-password-login

--- Auth - Secrets/app.js
+++ Auth - Secrets/app.js
@@ -5,9 +5,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-app.set("view engine" , "ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 main().then(async () => {
     await getRoutes();
@@ -17,29 +17,33 @@ async function main() {
     await mongoose.connect(process.env.DB_STR).catch(err => console.log(err));
 }
 
-async function getRoutes() {
+const getRoutes = async () => {
     const User = await createUserSchema();
 
-    app.get("/" , (req , res) => {
+    app.get("/", (req, res) => {
         res.render("home");
     });
 
-    app.get("/login" , (req , res) => {
+    app.get("/login", (req, res) => {
         res.render("login");
     });
 
-    app.get("/register" , (req , res) => {
+    app.post("/login" , async (req, res) => {
+        await loginUser(User.model , req.body.username , req.body.password , res);
+    });
+
+    app.get("/register", (req, res) => {
         res.render("register");
     });
 
-    app.post("/register" , async (req , res) => {
-
+    app.post("/register", async (req, res) => {
+        await registerUser(User.model, req.body.username, req.body.password, res);
     });
 }
 
 const createUserSchema = async () => {
     const userSchema = await new mongoose.Schema({
-        email : {
+        email: {
             type: String,
             required: true
         },
@@ -49,14 +53,35 @@ const createUserSchema = async () => {
         }
     });
 
-    const userModel = await new mongoose.model("User" , userSchema);
+    const userModel = await new mongoose.model("User", userSchema);
 
-    return {
+    return await {
         schema: userSchema,
         model: userModel
     }
 }
 
-app.listen(process.env.SERVER_PORT , () => {
+const registerUser = async (userModel, userEmail, userPassword, res) => {
+    const newUser = await new userModel({
+        email: userEmail,
+        password: userPassword
+    });
+
+    return await newUser.save().then(() => {
+        res.render("secrets");
+    }).catch(err => console.log(err));
+}
+
+const loginUser = async (userModel, userEmail, userPassword, res) => {
+    const foundUser = await userModel.findOne({ email: userEmail }).exec().then((foundUser) => {
+        if (foundUser) {
+            if (foundUser.password === userPassword) {
+                res.render("secrets");
+            }
+        }
+    }).catch(err => console.log(err));
+}
+
+app.listen(process.env.SERVER_PORT, () => {
     console.log("Server succesfully started on port 3000");
 })
